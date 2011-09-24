9e5f9fb25b1e711d95452e3488e6fcad1e2c3a32
Teng Siong Ong <siong1987@gmail.com>
2011-09-24 00:30:17 -0500

    New version that fixes missing templates to model.rb.
    Write test for this!!!! Fuck you.

--- lib/generators/mongoid_timeline_fu_generator.rb
+++ lib/generators/mongoid_timeline_fu_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators'
 
 class MongoidTimelineFuGenerator < Rails::Generators::Base
-  source_root File.expand_path("../../templates", __FILE__)
+  source_root File.expand_path("../templates", __FILE__)
 
   def create_migration_file
     copy_file 'model.rb', 'app/models/timeline_event.rb'
